ac58c2aedeb7beca9abb1582dd5eb44247de3dda
Dan Wright <djw7992@gmail.com>
2023-12-27 10:43:57 +0100

    test code

--- my_drone_ex.py
+++ my_drone_ex.py
@@ -2,6 +2,8 @@ import time
 import numpy as np
 import pybullet as p
 import matplotlib.pyplot as plt
+from rrt import rrt
+from rrt import Graph
 
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.utils.enums import DroneModel
@@ -14,11 +16,30 @@ env = CtrlAviary(gui=True)
 duration_sec = 10
 DRONE_MODEL = DroneModel("cf2x")
 
+# Test RRT stuff
+
+start = [1, 1, 1]
+
+goal = [100, 100, 100]
+
+graph = Graph(start, goal)
+
+while graph.goalReached != True:
+        
+        rrt(graph)
+        
+optimalNodes = graph.getOptimalPath()
+
 
 # The amount of waypoints in our trajectory, e.g. amount of RRT points
-NUM_WAYPOINTS = env.CTRL_FREQ*duration_sec
+NUM_WAYPOINTS = len(optimalNodes)
 TARGET_POS = np.zeros((NUM_WAYPOINTS,3)) # number of waypoints by x, y, z position
 PYB_CLIENT = env.getPyBulletClient()
+
+
+
+
+
 
 # Circle trajectory from the example, change this to our purpose
 R = .3
@@ -27,8 +48,8 @@ H_STEP = .05
 INIT_XYZS = np.array([[R*np.cos((i/6)*2*np.pi+np.pi/2), R*np.sin((i/6)*2*np.pi+np.pi/2)-R, H+i*H_STEP] for i in range(num_drones)])
 INIT_RPYS = np.array([[0, 0,  i * (np.pi/2)/num_drones] for i in range(num_drones)])
 
-for i in range(NUM_WAYPOINTS):
-    TARGET_POS[i, :] = R*np.cos((i/NUM_WAYPOINTS)*(2*np.pi)+np.pi/2)+INIT_XYZS[0, 0]+10, R*np.sin((i/NUM_WAYPOINTS)*(2*np.pi)+np.pi/2)-R+INIT_XYZS[0, 1], INIT_XYZS[0,2]-0.5
+for i in range(len(optimalNodes)):
+    TARGET_POS[i, :] = optimalNodes[i].x, optimalNodes[i].y, optimalNodes[i].z
 wp_counters = np.array([int((i*NUM_WAYPOINTS/6)%NUM_WAYPOINTS) for i in range(num_drones)]) # counter for where we are in the trajectory
 
 # Initialize action vector
@@ -48,6 +69,8 @@ for i in range(0, int(duration_sec*env.CTRL_FREQ)):
     # for drone in range(num_drones): 
         # action[drone,:] = 15000*np.array([[1., 1., 1., 1.]])
     for j in range(num_drones):
+        position = np.array((optimalNodes[j].x, optimalNodes[j].y, optissssmalNodes[j].z))
+        
         action[j, :], _, _ = ctrl[j].computeControlFromState(control_timestep=env.CTRL_TIMESTEP,
                                                                 state=obs[j],
                                                                 # target_pos=np.hstack([TARGET_POS[wp_counters[j], 0:2], INIT_XYZS[j, 2]]),
