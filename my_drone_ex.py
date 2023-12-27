61143605c33ce8b9a8905d48f3b8a7f537f84ea9
Jack Zeng <j.zeng-3@student.tudelft.nl>
2023-12-27 11:28:25 +0100

    added if statement for waypoint incrementation

--- my_drone_ex.py
+++ my_drone_ex.py
@@ -20,38 +20,33 @@ DRONE_MODEL = DroneModel("cf2x")
 
 start = [1, 1, 1]
 
-goal = [100, 100, 100]
+goal = [10, 10, 10]
 
 graph = Graph(start, goal)
 
 while graph.goalReached != True:
-        
         rrt(graph)
-        
 optimalNodes = graph.getOptimalPath()
 
+optimalNodes = np.flip(np.asarray(optimalNodes))
 
 # The amount of waypoints in our trajectory, e.g. amount of RRT points
 NUM_WAYPOINTS = len(optimalNodes)
 TARGET_POS = np.zeros((NUM_WAYPOINTS,3)) # number of waypoints by x, y, z position
 PYB_CLIENT = env.getPyBulletClient()
 
-
-
-
-
-
 # Circle trajectory from the example, change this to our purpose
 R = .3
 H = 1.
 H_STEP = .05
-INIT_XYZS = np.array([[R*np.cos((i/6)*2*np.pi+np.pi/2), R*np.sin((i/6)*2*np.pi+np.pi/2)-R, H+i*H_STEP] for i in range(num_drones)])
+# INIT_XYZS = np.array([[R*np.cos((i/6)*2*np.pi+np.pi/2), R*np.sin((i/6)*2*np.pi+np.pi/2)-R, H+i*H_STEP] for i in range(num_drones)])
+INIT_XYZS = np.array(start)
 INIT_RPYS = np.array([[0, 0,  i * (np.pi/2)/num_drones] for i in range(num_drones)])
 
 for i in range(len(optimalNodes)):
     TARGET_POS[i, :] = optimalNodes[i].x, optimalNodes[i].y, optimalNodes[i].z
 wp_counters = np.array([int((i*NUM_WAYPOINTS/6)%NUM_WAYPOINTS) for i in range(num_drones)]) # counter for where we are in the trajectory
-
+print(f"TARGET POSITION{TARGET_POS}")
 # Initialize action vector
 action = np.zeros((num_drones, 4)) # number of drones by the amount of actions, 1 RPM for each propellor
 
@@ -67,10 +62,7 @@ for i in range(0, int(duration_sec*env.CTRL_FREQ)):
 
     # For every drone calculate the action based on the current observation (control for current waypoint)
     # for drone in range(num_drones): 
-        # action[drone,:] = 15000*np.array([[1., 1., 1., 1.]])
     for j in range(num_drones):
-        position = np.array((optimalNodes[j].x, optimalNodes[j].y, optissssmalNodes[j].z))
-        
         action[j, :], _, _ = ctrl[j].computeControlFromState(control_timestep=env.CTRL_TIMESTEP,
                                                                 state=obs[j],
                                                                 # target_pos=np.hstack([TARGET_POS[wp_counters[j], 0:2], INIT_XYZS[j, 2]]),
@@ -79,9 +71,12 @@ for i in range(0, int(duration_sec*env.CTRL_FREQ)):
                                                                 )
 
     # increment to the next waypoint
+    distance = np.sqrt((obs[j][0] - TARGET_POS[wp_counters[j], 0])**2 + (obs[j][1] - TARGET_POS[wp_counters[j], 1])**2 + (obs[j][2] - TARGET_POS[wp_counters[j], 2])**2)
+    print(f"DISTANCE {distance}")
     for j in range(num_drones):
-            wp_counters[j] = wp_counters[j] + 1 if wp_counters[j] < (NUM_WAYPOINTS-1) else 0
-
+            if distance < 5:
+                wp_counters[j] = wp_counters[j] + 1 if wp_counters[j] < (NUM_WAYPOINTS-1) else wp_counters[-1]
+            print("waypoint counter",wp_counters)
     time.sleep(1. / env.CTRL_FREQ)
 env.close()
 
